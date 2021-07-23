edba9b40146382b326a0e5fe20963c24f54cc3c6
tejothanmaya <83295627+tejothanmaya@users.noreply.github.com>
2021-07-23 15:34:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,78 @@
+
+  var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+var database;
+var position;
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  textSize(20); 
+
+var ballpos = database.ref('balloon/position')
+ballpos.on('value',readposition,showerror)
+
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    changePosition(-10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    changePosition(10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+  }
+  else if(keyDown(UP_ARROW)){
+    changePosition(0,-10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale -0.005;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    changePosition(0,+10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale+0.005;
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+
+function changePosition(x,y){
+  database.ref('balloon/position').set({
+    x: position.x + x ,
+    y: position.y + y
+  })
+}
+
+function readposition(data){
+
+position = data.val()
+balloon.x = position.x
+balloon.y = position.y
+
+}
+function showerror(){
+console.log("Error occurred check your code")
+
+}
